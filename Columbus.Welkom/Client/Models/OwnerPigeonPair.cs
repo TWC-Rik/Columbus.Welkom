6259961cac181e14041c5ffe2a27cfa2f7215ba2
Rik van den berg <rvdberg-@hotmail.com>
2023-07-22 14:48:48 +0200

    SelectedYearPigeon and PigeonSwap Views
    UI mostly in place.

---
+++ Columbus.Welkom/Client/Models/OwnerPigeonPair.cs
@@ -0,0 +1,29 @@
+﻿using Columbus.Models;
+
+namespace Columbus.Welkom.Client.Models
+{
+    public class OwnerPigeonPair
+    {
+        public OwnerPigeonPair()
+        {
+
+        }
+
+        public OwnerPigeonPair(Owner owner, Pigeon pigeon)
+        {
+            Owner = owner;
+            Pigeon = pigeon;
+        }
+
+        public OwnerPigeonPair(Owner owner, Pigeon pigeon, int points)
+        {
+            Owner = owner;
+            Pigeon = pigeon;
+            Points = points;
+        }
+
+        public Owner? Owner { get; set; }
+        public Pigeon? Pigeon { get; set; }
+        public int Points { get; set; }
+    }
+}
---
+++ Columbus.Welkom/Client/Models/PigeonSwapPair.cs
@@ -0,0 +1,32 @@
+﻿using Columbus.Models;
+
+namespace Columbus.Welkom.Client.Models
+{
+    public class PigeonSwapPair
+    {
+        public PigeonSwapPair()
+        {
+
+        }
+
+        public PigeonSwapPair(Owner owner, Pigeon pigeon, Owner coupledOwner)
+        {
+            Owner = owner;
+            Pigeon = pigeon;
+            CoupledOwner = coupledOwner;
+        }
+
+        public PigeonSwapPair(Owner owner, Pigeon pigeon, Owner coupledOwner, int points)
+        {
+            Owner = owner;
+            Pigeon = pigeon;
+            CoupledOwner = coupledOwner;
+            Points = points;
+        }
+        
+        public Owner? Owner { get; set; }
+        public Pigeon? Pigeon { get; set; }
+        public Owner? CoupledOwner { get; set; }
+        public int Points { get; set; }
+    }
+}
